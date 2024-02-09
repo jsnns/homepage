70dd2a32fecc2489f97b0373bc27140b695bc197
Jacob Sansbury <sansburyjacob@gmail.com>
2024-02-09 11:11:27 -0800

    Layout fixes

--- src/components/BlogLayout.tsx
+++ src/components/BlogLayout.tsx
@@ -1,9 +1,14 @@
 import "@/app/globals.css";
+import RootLayout from "@/app/layout";
 
 export const BlogLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  return <div className="flex flex-col p-8 md:p-24 max-w-4xl">{children}</div>;
+  return (
+    <RootLayout>
+      <div className="flex flex-col p-8 md:p-24 max-w-4xl">{children}</div>
+    </RootLayout>
+  );
 };
